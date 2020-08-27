0d7961a079a80547d6a4fd602fb80f49f481ce68
RobRukavina <rehabrob440@gmail.com>
2020-08-27 00:16:44 -0600

    Adding C# Bubble Sort

--- C#BubbleSort.cs
+++ C#BubbleSort.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+
+// C# Bubble Sort
+
+public class BubbleSortExample
 {
-	public Class1()
+	static void bubbleSort( int []arr)
 	{
-		//
-		// TODO: Add constructor logic here
-		//
+		int n = arr.Length;
+		for (int i = 0; i< n - 1; i++)
+			for (int j = 0; j < n - i -1; j++)
+				if (arr[j] > arr[j + 1])
+                {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+                }
 	}
+
+	static void printArray(int []arr)
+    {
+		int n = arr.Length;
+		for (int i = 0; i < n; i++)
+			Console.Write(arr[i] + " ");
+		Console.WriteLine();
+    }
+
+	public static void Main()
+    {
+		int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+		bubbleSort(arr);
+		Console.WriteLine("Sorted Array");
+		printArray(arr);
+    }
 }
